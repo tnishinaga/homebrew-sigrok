2ab0fa2a7ba0fdaeb95f9ccfedc7a77bee49888a
Toshifumi NISHINAGA <tnishinaga.dev@gmail.com>
2021-09-13 04:11:33 +0900

    wip: support head

--- libsigrokdecode.rb
+++ libsigrokdecode.rb
@@ -3,10 +3,9 @@
 class Libsigrokdecode < Formula
   desc "Shared library providing protocol decoding functionality"
   homepage "https://sigrok.org/wiki/Libsigrokdecode"
-  # url "https://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.5.3.tar.gz"
-  url "git://sigrok.org/libsigrokdecode"
-  version "HEAD"
-  # sha256 "c50814aa6743cd8c4e88c84a0cdd8889d883c3be122289be90c63d7d67883fc0"
+  url "https://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.5.3.tar.gz"
+  head "git://sigrok.org/libsigrokdecode"
+  sha256 "c50814aa6743cd8c4e88c84a0cdd8889d883c3be122289be90c63d7d67883fc0"
 
   # glib-2.0 >= 2.32.0
   depends_on "automake" => :build
@@ -23,43 +22,43 @@ class Libsigrokdecode < Formula
 
     system "sed", "-i", "-e", "s/@SRD_PKGLIBS@/python3-embed/g", "libsigrokdecode.pc.in"
     system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}"
-    system "make"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules", "--prefix=#{prefix}"
     system "make", "install"
-    system "make", "install-decoders"
+    system "make", "install", "install-decoders"
   end
 
-  # test do
-  #   (testpath/"test.c").write <<~EOS
-  #     #include <stdio.h>
-  #     #include <libsigrokdecode/libsigrokdecode.h>
-  #     #include <glib.h>
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <libsigrokdecode/libsigrokdecode.h>
+      #include <glib.h>
 
-  #     int main(int argc, char **argv)
-  #     {
-  #       int ret;
-  #       if ((ret = srd_init(NULL)) != SRD_OK) {
-  #         printf("Error initializing libsigrokdecode (%s): %s.",
-  #                 srd_strerror_name(ret), srd_strerror(ret));
-  #         return 1;
-  #       }
-  #       srd_decoder_load_all();
-  #       const GSList* decoders = srd_decoder_list();
-  #       if (!decoders) {
-  #         printf("Error listing decoders");
-  #         return 1;
-  #       };
-  #       guint num_decoders = g_slist_length((GSList *)decoders);
-  #       if (num_decoders == 0) {
-  #         printf("No decoders listed");
-  #         return 1;
-  #       };
-  #       return 0;
-  #     }
-  #   EOS
-  #   pkg_config_flags = `pkg-config --cflags --libs glib-2.0`.chomp.split
-  #   system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lsigrokdecode",
-  #                  *pkg_config_flags, "-o", "test"
-  #   system "./test"
-  # end
+      int main(int argc, char **argv)
+      {
+        int ret;
+        if ((ret = srd_init(NULL)) != SRD_OK) {
+          printf("Error initializing libsigrokdecode (%s): %s.",
+                  srd_strerror_name(ret), srd_strerror(ret));
+          return 1;
+        }
+        srd_decoder_load_all();
+        const GSList* decoders = srd_decoder_list();
+        if (!decoders) {
+          printf("Error listing decoders");
+          return 1;
+        };
+        guint num_decoders = g_slist_length((GSList *)decoders);
+        if (num_decoders == 0) {
+          printf("No decoders listed");
+          return 1;
+        };
+        return 0;
+      }
+    EOS
+    pkg_config_flags = `pkg-config --cflags --libs glib-2.0`.chomp.split
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lsigrokdecode",
+                   *pkg_config_flags, "-o", "test"
+    system "./test"
+  end
 end
